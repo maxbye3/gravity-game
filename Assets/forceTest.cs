8922898601bb31421c5b2f6c910d20f8fc3fb2da
Max Bye <mbye@techsoup.org>
2020-03-24 09:49:11 +0000

    simple grav test

---
+++ Assets/forceTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class forceTest : MonoBehaviour
+{
+    public int impactStrength = 10;
+    public GameObject[] planets;
+    public float gravity = 10;
+    protected Rigidbody bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+      bullet = GetComponent<Rigidbody>();
+      bullet.AddForce(Vector2.right * impactStrength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      planets = GameObject.FindGameObjectsWithTag("Planet");
+      foreach (GameObject planet in planets)
+        {
+          Vector3 direction = planet.transform.position - transform.position;
+          bullet.AddForce(gravity * direction);
+          // Debug.Log("Distance to planet: " + direction);
+        }
+        
+    }
+}
