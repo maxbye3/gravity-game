6913414817eb68d2cd84a1ad247c2f5607de5b2d
Max Bye <mbye@techsoup.org>
2020-04-02 10:44:26 +0100

    button click

---
+++ Assets/Scripts/UI/editPlanetBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class editPlanetBehaviour : MonoBehaviour
+{
+   int n;
+   public void OnButtonPress(){
+    n++;
+    Debug.Log("Button clicked " + n + " times.");
+   }
+   public void onButtonEnter(){
+    GameObject.FindGameObjectWithTag("GameController").GetComponent<gameStates>().gameState = "ui"; // We are in game state (not intro)
+    Debug.Log("Button enter");
+   }
+   public void onButtonExit(){
+    Debug.Log("Button exit");
+    GameObject.FindGameObjectWithTag("GameController").GetComponent<gameStates>().gameState = "game"; // We are in game state (not intro)
+   }
+}
